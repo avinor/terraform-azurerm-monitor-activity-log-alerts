f068c5dcd2385c4eff36b17ceb5f2a78539d484e
Øyvind Strømmen <insula.ventus@gmail.com>
2022-05-23 09:49:35 +0200

    Use separate action groups for each activity log alert
    Also, move http trigger schema and webhook http body out of module. This
     so that clients can use custom webhooks for each activity log category.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.98.0"
+      version = "~> 3.6.0"
     }
   }
 }
@@ -13,8 +13,10 @@ provider azurerm {
 }
 
 data "azurerm_key_vault_secret" "kvs" {
-  name         = var.webhook.service_uri
-  key_vault_id = var.webhook.key_vault_id
+  for_each = {for k, v in var.activity_log_alerts : k => v if v.action_group.logic_app.webhook.key_vault_id != null}
+
+  name         = each.value.action_group.logic_app.webhook.uri
+  key_vault_id = each.value.action_group.logic_app.webhook.key_vault_id
 }
 
 resource "azurerm_resource_group" "main" {
@@ -25,7 +27,9 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_logic_app_workflow" "la" {
-  name                = "${var.name}-la-workflow"
+  for_each = {for k, v in var.activity_log_alerts : k => v if v.action_group.logic_app != null}
+
+  name                = "${replace(each.key, "_", "-")}-la-workflow"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -33,125 +37,54 @@ resource "azurerm_logic_app_workflow" "la" {
 }
 
 resource "azurerm_logic_app_trigger_http_request" "request" {
-  name         = "${var.name}-la-alert-trigger-http"
-  logic_app_id = azurerm_logic_app_workflow.la.id
+  for_each = {for k, v in var.activity_log_alerts : k => v if v.action_group.logic_app != null}
 
-  schema = <<SCHEMA
-{
-    "type": "object",
-    "properties": {
-        "schemaId": {
-            "type": "string"
-        },
-        "data": {
-            "type": "object",
-            "properties": {
-                "essentials": {
-                    "type": "object",
-                    "properties": {
-                        "alertId": {
-                            "type": "string"
-                        },
-                        "alertRule": {
-                            "type": "string"
-                        },
-                        "severity": {
-                            "type": "string"
-                        },
-                        "signalType": {
-                            "type": "string"
-                        },
-                        "monitorCondition": {
-                            "type": "string"
-                        },
-                        "monitoringService": {
-                            "type": "string"
-                        },
-                        "alertTargetIDs": {
-                            "type": "array",
-                            "items": {
-                                "type": "string"
-                            }
-                        },
-                        "originAlertId": {
-                            "type": "string"
-                        },
-                        "firedDateTime": {
-                            "type": "string"
-                        },
-                        "resolvedDateTime": {
-                            "type": "string"
-                        },
-                        "description": {
-                            "type": "string"
-                        },
-                        "essentialsVersion": {
-                            "type": "string"
-                        },
-                        "alertContextVersion": {
-                            "type": "string"
-                        }
-                    }
-                },
-                "alertContext": {
-                    "type": "object",
-                    "properties": {}
-                }
-            }
-        }
-    }
-}
-SCHEMA
+  name         = "${replace(each.key, "_", "-")}-la-alert-trigger-http"
+  logic_app_id = azurerm_logic_app_workflow.la[each.key].id
 
+  schema = each.value.action_group.logic_app.http_trigger_schema
 }
 
 resource "azurerm_logic_app_action_http" "action" {
-  name         = "${var.name}-la-action"
-  logic_app_id = azurerm_logic_app_workflow.la.id
+  for_each = {for k, v in var.activity_log_alerts : k => v if v.action_group.logic_app != null}
+
+  name         = "${replace(each.key, "_", "-")}-la-action"
+  logic_app_id = azurerm_logic_app_workflow.la[each.key].id
   method       = "POST"
-  uri          = var.webhook.key_vault_id == null ? var.webhook.service_uri : data.azurerm_key_vault_secret.kvs.value
-  headers = {
+  uri          = each.value.action_group.logic_app.webhook.key_vault_id == null ? each.value.action_group.logic_app.webhook.uri : data.azurerm_key_vault_secret.kvs[each.key].value
+  body         = each.value.action_group.logic_app.webhook.body
+  headers      = {
     "Content-type" = "application/json"
   }
-  body = <<-BODY
-  {
-      "username": "AzureHealth - Activity Log Alert",
-      "blocks": [
-          {
-              "type": "section",
-              "text": {
-                  "type": "mrkdwn",
-                  "text": "New Activity Log Alert: https://portal.azure.com/#blade/Microsoft_Azure_Health/AzureHealthBrowseBlade/serviceIssues\n\nDetails:\nMonitoring Service: @{triggerBody()?['data']?['essentials']?['monitoringService']}\nMonitoring Condition: @{triggerBody()?['data']?['essentials']?['monitorCondition']}\nDescription: @{triggerBody()?['data']?['essentials']?['description']}\nSignal Type: @{triggerBody()?['data']?['essentials']?['signalType']}\nSeverity: @{triggerBody()?['data']?['essentials']?['severity']}\nAlert Rule: @{triggerBody()?['data']?['essentials']?['alertRule']}\nAlert ID: @{triggerBody()?['data']?['essentials']?['alertId']}\nAlert Target IDs: @{triggerBody()?['data']?['essentials']?['alertTargetIDs']}\n"
-              }
-          }
-      ]
-  }
-  BODY
 }
 
 resource "azurerm_monitor_action_group" "main" {
-  name                = "${var.name}-ag"
+  for_each = var.activity_log_alerts
+
+  name                = "${each.value.action_group.name}-ag"
   resource_group_name = azurerm_resource_group.main.name
-  short_name          = var.short_name
+  short_name          = substr(each.value.action_group.display_name, 0, 12)
 
   tags = var.tags
 
   dynamic "email_receiver" {
-    for_each = var.emails
+    for_each = each.value.action_group.email != null ? [true] : []
     content {
-      name                    = email_receiver.value.name
-      email_address           = email_receiver.value.email_address
-      use_common_alert_schema = email_receiver.value.use_common_alert_schema
+      name                    = each.value.action_group.email.name
+      email_address           = each.value.action_group.email.address
+      use_common_alert_schema = each.value.action_group.email.use_common_alert_schema
     }
   }
 
-  logic_app_receiver {
-    name                    = "${var.name}-la-action"
-    resource_id             = azurerm_logic_app_workflow.la.id
-    callback_url            = azurerm_logic_app_trigger_http_request.request.callback_url
-    use_common_alert_schema = true
+  dynamic "logic_app_receiver" {
+    for_each = each.value.action_group.logic_app != null ? [true] : []
+    content {
+      name                    = "${replace(each.key, "_", "-")}-la-action"
+      resource_id             = azurerm_logic_app_workflow.la[each.key].id
+      callback_url            = azurerm_logic_app_trigger_http_request.request[each.key].callback_url
+      use_common_alert_schema = true
+    }
   }
-
 }
 
 resource "azurerm_monitor_activity_log_alert" "main" {
@@ -159,22 +92,25 @@ resource "azurerm_monitor_activity_log_alert" "main" {
 
   tags = var.tags
 
-  name                = each.key
+  name                = replace(each.key, "_", "-")
   resource_group_name = azurerm_resource_group.main.name
   scopes              = each.value.scopes
   description         = each.value.description
 
   criteria {
-    category = each.value.criteria_category
+    category = each.value.category
     dynamic "service_health" {
-      for_each = each.value.criteria_category == "ServiceHealth" && each.value.criteria_locations != null ? [true] : []
+      for_each = each.value.category == "ServiceHealth" && each.value.regions != null ? [true] : []
       content {
-        locations = each.value.criteria_locations
+        locations = each.value.regions
+        events    = []
+        services  = []
       }
     }
   }
 
   action {
-    action_group_id = azurerm_monitor_action_group.main.id
+    action_group_id    = azurerm_monitor_action_group.main[each.key].id
+    webhook_properties = {}
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "name" {
-  description = "The name of the Action Group."
-}
-
 variable "resource_group_name" {
   description = "Name of resource group to deploy resources in."
 }
@@ -10,50 +6,36 @@ variable "location" {
   description = "The Azure Region in which to create resource."
 }
 
-variable "short_name" {
-  description = "The short name of the action group. This will be used in SMS messages."
-}
-
-variable "enabled" {
-  description = "Whether this action group is enabled. If an action group is not enabled, then none of its receivers will receive communications. Defaults to true."
-  type        = bool
-  default     = true
-}
-
 variable "tags" {
   description = "Tags to apply to all resources created."
   type        = map(string)
   default     = {}
 }
 
-
-variable "emails" {
-  description = "List of email receivers"
-  type = list(object({
-    name                    = string
-    email_address           = string
-    use_common_alert_schema = bool
-  }))
-  default = []
-}
-
-variable "webhook" {
-  description = "List of webhook receivers. If key_vault_id is set service_uri is key vault secret name"
-  type = object({
-    name                    = string
-    key_vault_id            = string
-    service_uri             = string
-    use_common_alert_schema = bool
-  })
-}
-
 variable "activity_log_alerts" {
   description = "Map of activity log alerts"
-  type = map(object({
-    scopes             = list(string)
-    description        = string
-    criteria_category  = string
-    criteria_locations = list(string)
+  type        = map(object({
+    scopes       = list(string)
+    description  = string
+    category     = string
+    regions      = list(string)
+    action_group = object({
+      name         = string
+      display_name = string
+      logic_app    = object({
+        http_trigger_schema = string
+        webhook             = object({
+          key_vault_id = string
+          uri          = string
+          body         = string
+        })
+      })
+      email = object({
+        name                    = string
+        address                 = string
+        use_common_alert_schema = bool
+      })
+    })
   }))
   default = null
 }
